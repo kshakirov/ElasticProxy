fdd11d8967259bae0fa9f536c8d32644854077ab
Kirill Shakirov <kirill.shakirov4@gmail.com>
2016-05-19 16:20:41 +0300

    WIP added screen removed hardcoded type into proxy elastic

--- proxy/proxy.rb
+++ proxy/proxy.rb
@@ -5,14 +5,14 @@ require_relative 'proxy/price_manager'
 require_relative 'proxy/decriptor'
 
 
-#set :bind, '0.0.0.0'
+set :bind, '0.0.0.0'
 set :port, 4569
 
 
 post '/magento_product/_search' do
   @json = JSON.parse(request.body.read)
-  client = Elasticsearch::Client.new   host: 'localhost', log: true
-  response =client.search index: 'magento_product', type: 'product', size: @params[:size],  from: @params[:from], body: @json
+  client = Elasticsearch::Client.new   host: '10.0.3.108', log: true
+  response =client.search index: 'magento_product', size: @params[:size],  from: @params[:from], body: @json
   price_manager = PriceManager.new
   price_manager.get_simple_price response,@params['stats']
   response.to_json
