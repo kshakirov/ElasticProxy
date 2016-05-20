679a7fe3073106c40136c23fb69f996b1bf00db3
kshakirov <kshakirov@zoral.com.ua>
2016-05-20 14:25:10 +0300

    WIP client once initialized

--- proxy/proxy.rb
+++ proxy/proxy.rb
@@ -5,16 +5,23 @@ require_relative 'proxy/price_manager'
 require_relative 'proxy/decriptor'
 
 
+configure do
+  set :client, Elasticsearch::Client.new(host: 'localhost', log: true)
+end
+
 #set :bind, '0.0.0.0'
 set :port, 4569
 
 
 post '/magento_product/_search' do
   @json = JSON.parse(request.body.read)
-  client = Elasticsearch::Client.new   host: 'localhost', log: true
-  response =client.search index: 'magento_product', type: 'product', size: @params[:size],  from: @params[:from], body: @json
+  response =settings.client.search index: 'magento_product', size: @params[:size],  from: @params[:from], body: @json
   price_manager = PriceManager.new
   price_manager.get_simple_price response,@params['stats']
   response.to_json
 
+end
+
+get '/test' do
+  puts "it is test"
 end
