7da6c73f0bada4efd7b2d3f593de496ad53b3495
kshakirov <kshakirov@zoral.com.ua>
2016-07-15 13:26:47 +0300

    added manufacturer route

--- proxy/proxy.rb
+++ proxy/proxy.rb
@@ -87,3 +87,11 @@ get '/critical/index/partsheaders' do
   uri = "/critical/index/partsheaders?part_type=" + params[:part_type]
   settings.magento_proxy.create_cached_response uri, settings.redis_client, settings.host
 end
+
+get '/critical/index/manufacturersfilters' do
+  content_type 'application/json'
+  cache_control :public, :max_age => 360000000
+  expires 5000000000, :public
+  uri = "/critical/index/manufacturersfilters?part_type=" + params[:part_type] + '&manufacturer=' + params[:manufacturer]
+  settings.magento_proxy.create_cached_response uri, settings.redis_client, settings.host
+end
