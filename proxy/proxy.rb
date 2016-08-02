47cfd1c14ff2e094bc2f65221155a819935b3e7e
kshakirov <kshakirov@zoral.com.ua>
2016-08-02 13:21:35 +0300

    WIP #1 added featured and new products

--- proxy/proxy.rb
+++ proxy/proxy.rb
@@ -8,8 +8,6 @@ require_relative 'proxy/price_manager'
 require_relative 'proxy/decriptor'
 require_relative 'proxy/magento_proxy'
 
-use Rack::Cache
-
 configure do
   set :client, Elasticsearch::Client.new(host: 'elastic-instance', log: true)
   set :redis_client, Redis.new(:host => "redis")
@@ -25,6 +23,12 @@ before do
   headers 'Connection' => 'permanent'
 end
 
+def set_critical_cache
+  content_type 'application/json'
+  cache_control :public, :max_age => 360000000
+  expires 5000000000, :public
+end
+
 
 post '/magento_product/_search' do
   @json = JSON.parse(request.body.read)
@@ -35,72 +39,71 @@ post '/magento_product/_search' do
 
 end
 
-get '/test' do
-  puts "it is test"
-end
 
 get '/critical/index/sorters' do
-  content_type 'application/json'
-  cache_control :public, :max_age => 360000000
-  expires 5000000000, :public
+  set_critical_cache
   uri = "/critical/index/sorters?part_type=" + params[:part_type]
   settings.magento_proxy.create_cached_response uri, settings.redis_client, settings.host
 end
 
 get '/critical/index/headers' do
-  content_type 'application/json'
-  cache_control :public, :max_age => 360000000
-  expires 5000000000, :public
+  set_critical_cache
   uri = "/critical/index/headers?part_type=" + params[:part_type]
   settings.magento_proxy.create_cached_response uri, settings.redis_client, settings.host
 end
 
 
 get '/critical/index/filters' do
-  content_type 'application/json'
-  cache_control :public, :max_age => 360000000
-  expires 5000000000, :public
+  set_critical_cache
   uri = "/critical/index/filters?part_type=" + params[:part_type]
   settings.magento_proxy.create_cached_response uri, settings.redis_client, settings.host
 end
 
 get '/critical/index/partssorters' do
-  content_type 'application/json'
-  cache_control :public, :max_age => 360000000
-  expires 5000000000, :public
+  set_critical_cache
   uri = "/critical/index/partssorters?part_type=" + params[:part_type]
   settings.magento_proxy.create_cached_response uri, settings.redis_client, settings.host
 end
 
 get '/critical/index/partsfilters' do
-  content_type 'application/json'
-  cache_control :public, :max_age => 360000000
-  expires 5000000000, :public
+  set_critical_cache
   uri = "/critical/index/partsfilters?part_type=" + params[:part_type]
   settings.magento_proxy.create_cached_response uri, settings.redis_client, settings.host
 end
 
 get '/critical/index/partsheaders' do
-  content_type 'application/json'
-  cache_control :public, :max_age => 360000000
-  expires 5000000000, :public
+  set_critical_cache
   uri = "/critical/index/partsheaders?part_type=" + params[:part_type]
   settings.magento_proxy.create_cached_response uri, settings.redis_client, settings.host
 end
 
 get '/critical/index/manufacturersfilters' do
-  content_type 'application/json'
-  cache_control :public, :max_age => 360000000
-  expires 5000000000, :public
+  set_critical_cache
   uri = "/critical/index/manufacturersfilters?part_type=" + params[:part_type] + '&manufacturer=' + params[:manufacturer]
   settings.magento_proxy.create_cached_response uri, settings.redis_client, settings.host
 end
 
 
 get '/critical/index/catalogfilters' do
-  content_type 'application/json'
-  cache_control :public, :max_age => 360000000
-  expires 5000000000, :public
+  set_critical_cache
   uri = "/critical/index/catalogfilters"
   settings.magento_proxy.create_cached_response uri, settings.redis_client, settings.host
+end
+
+get '/critical/index/featuredProducts' do
+  set_critical_cache
+  uri = "/critical/index/featuredProducts?stats=" + params[:stats]
+  settings.magento_proxy.create_cached_response uri, settings.redis_client, settings.host
+end
+
+get '/critical/index/newProducts' do
+  set_critical_cache
+  uri = "/critical/index/newProducts?stats=" + params[:stats]
+  settings.magento_proxy.create_cached_response uri, settings.redis_client, settings.host
+end
+
+
+if ENV['MAGENTO_HOST'].nil?
+  puts 'Set MAGENTO_HOST env variable, which must point to Magento Host'
+  exit(1)
 end
