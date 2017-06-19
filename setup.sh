2d20a6bc1f187cec7fb039ac64008c32ffba1d4b
kshakirov <kshakirov@zoral.com.ua>
2017-06-19 13:34:30 +0300

    test

--- setup.sh
+++ setup.sh
@@ -7,4 +7,5 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 \curl -sSL https://get.rvm.io | bash -s stable
 source /home/ubuntu/.rvm/scripts/rvm
 rvm install jruby
+
 gem install bundler
